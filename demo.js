23de9d48dc6e571b5ddd1a03b7a63853fd4427df
unknown <you@example.com>
2023-05-01 22:52:32 +0700

    add web file

---
+++ demo.js
@@ -0,0 +1,35 @@
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // Get the root element
+var r = document.querySelector(':root');
+
+// Create a function for getting a variable value
+function myFunction_get() {
+    // Get the styles (properties and values) for the root
+    var rs = getComputedStyle(r);
+    // Alert the value of the --blue variable
+    alert("The value of --red is: " + rs.getPropertyValue('--red'));
+}
+
+// Create a function for setting a variable value
+function myFunction_set() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--red', 'blue');
+}
